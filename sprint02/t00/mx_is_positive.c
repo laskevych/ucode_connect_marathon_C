0c0de5521e927d5fb4ecebc1b27b658261562b65
Andrew Laskevych <an.laskevych@gmail.com>
2023-01-28 21:41:07 +0200

    Add t00

---
+++ sprint02/t00/mx_is_positive.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void mx_printstr(const char *s);
+
+void mx_is_positive(int i) {
+    if (i > 0) {
+        mx_printstr("positive\n");
+    }
+    else {
+        if (i < 0) {
+            mx_printstr("negative\n");
+        }
+        else {
+            mx_printstr("zero\n");
+        }
+    }
+}
+
