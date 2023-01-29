803a9aaaec2fb5dcf330b13cc12d8f16eff1d0a8
Andrew Laskevych <an.laskevych@gmail.com>
2023-01-29 08:58:26 +0200

    Add t14

---
+++ sprint02/t14/mx_printint.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void mx_printchar(char c);
+
+void mx_printint(int n) {
+    char data[55];
+    long number = n;
+    int i = 0;
+
+    if (number == 0) {
+        mx_printchar(n + '0');
+    }
+
+    if (number < 0) {
+        number *= -1;
+        mx_printchar('-'); 
+    }
+
+    while (number != 0) {
+        data[i++] = (number % 10) + '0';
+        number = number / 10;
+    }
+
+    for (int j = i - 1; j >= 0; j--) {
+        mx_printchar(data[j]);
+    }
+}
+
