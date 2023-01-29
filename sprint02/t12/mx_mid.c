30855c6755d13a5d417d77025d54fd4e793a51ff
Andrew Laskevych <an.laskevych@gmail.com>
2023-01-29 08:58:08 +0200

    Add t12

---
+++ sprint02/t12/mx_mid.c
@@ -0,0 +1,14 @@
+int mx_mid(int a, int b, int c) {
+    if ((a >= b && a <= c)
+        || (a <= b && a >= c)) {
+        return a;
+    }
+    else if ((b >= a && b <= c)
+        || (b <= a && b >= c)) {
+        return b;
+    }
+    else {
+        return c;
+    }
+}
+
