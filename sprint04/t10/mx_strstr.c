7c849b036da8b91fe07fb5a397ac66777027377a
Andrew Laskevych <an.laskevych@gmail.com>
2023-02-02 21:47:36 +0200

    Add task10

---
+++ sprint04/t10/mx_strstr.c
@@ -0,0 +1,16 @@
+int mx_strlen(const char *s);
+int mx_strncmp(const char *s1, const char *s2, int n);
+char *mx_strchr(const char *s, int c);
+
+char *mx_strstr(const char *s1, const char *s2) {
+    while (*s1 != '\0') {
+        if (!mx_strncmp(s1, s2, mx_strlen(s2))) {
+            return (char *)s1;
+        }
+
+        s1++;
+    }
+
+    return 0;
+}
+
