2b6f54e6299c53efdfa474cba17072ff295fba4d
Andrew Laskevych <an.laskevych@gmail.com>
2023-02-02 21:46:56 +0200

    Add task04

---
+++ sprint04/t04/mx_strcat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int mx_strlen(const char *s);
+
+char *mx_strcat(char *s1, const char *s2){
+    char* str = s1;
+    int length_1 = mx_strlen(s1);
+    int length_2 = mx_strlen(s2);
+    
+    if ((s1 != NULL)
+        && (s2 != NULL)) {
+        while (length_1--) {
+            s1++;
+        }
+
+        while (length_2--) {
+            *s1++ = *s2++;
+        }
+        *s1 = '\0';
+    }
+
+    return str;
+}
+
