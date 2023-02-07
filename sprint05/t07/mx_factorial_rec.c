724005ee9d4c29dc4ab7ab81be9e1a494220bb15
Andrew Laskevych <an.laskevych@gmail.com>
2023-02-07 10:53:42 +0200

    Add t07

---
+++ sprint05/t07/mx_factorial_rec.c
@@ -0,0 +1,21 @@
+int mx_factorial_rec(int n) {
+    if (n < 0
+        || n > 12) {
+        return 0;
+    }
+    
+    if (n == 0) {
+        return 1;
+    }
+
+    int res = n * mx_factorial_rec(n - 1);
+
+    if (res > 2147483647
+        || res < 0) {
+        return 0;
+    }
+    else {
+        return res;
+    }
+}
+
