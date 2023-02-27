c683fb42de7115e62cfe89097bf66769c0b7aff9
Andrew Laskevych <an.laskevych@gmail.com>
2023-02-27 12:02:04 +0200

    Add t00

---
+++ sprint08/t00/header.h
@@ -0,0 +1,18 @@
+#ifndef HEADER
+#define HEADER
+
+#include <stdbool.h>
+#include <unistd.h>
+
+void mx_printchar(char c);
+void mx_printint(int s);
+void mx_printstr(const char *s);
+char *mx_strcpy(char *dst, const char *src);
+int mx_strlen(const char *s);
+int mx_strcmp(const char *s1, const char *s2);
+bool mx_isdigit(int c);
+bool mx_isspace(char c);
+int mx_atoi(const char *str);
+
+#endif
+
