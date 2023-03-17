e8d75ad1c547cab734d20222995000582f00802a
Andrew Laskevych <an.laskevych@gmail.com>
2023-03-17 09:44:45 +0200

    Add t02

---
+++ sprint09/t02/src/mx_atoi.c
@@ -0,0 +1,45 @@
+#include "../inc/header.h"
+
+int mx_atoi(const char *str) {
+    int start = -1;
+    int end = -1;
+    int result = 0;
+    int sign = 1;
+
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] == 45
+            && mx_isdigit(str[i + 1])) {
+            sign = -1;
+            i++;
+            continue;
+        }
+
+        if (!mx_isspace(str[i])
+            && start == -1) {
+            start = i;
+        }
+
+        if (start > -1
+            && !mx_isdigit(str[i])
+            && end == -1) {
+            end = i;
+        }
+
+        i++;
+    }
+
+    if (end == -1) {
+        for (int i = start; str[i] != '\0'; i++) {
+            result = (result * 10) + (int)str[i] - 48;
+        }
+    } else {
+        for (int i = start; i < end; i++) {
+            result = (result * 10) + (int)str[i] - 48;
+        }
+    }
+
+
+    return result * sign;
+}
+
